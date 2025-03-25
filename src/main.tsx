fc7e673b7d75abe6d4c5faebb272e5e35e9e9157
Panachai <panachai.ny@gmail.com>
2025-03-26 00:03:57 +0700

    add useQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 
 const root = document.getElementById('root')
@@ -9,8 +10,12 @@ if (!root) {
   throw new Error('No root element found')
 }
 
+const queryClient = new QueryClient()
+
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
