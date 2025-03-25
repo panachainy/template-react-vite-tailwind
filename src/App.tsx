6d51bd7ab916a38a2221d73818e172840eabe5df
Panachai <panachai.ny@gmail.com>
2025-03-26 00:15:57 +0700

    add pokemon page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import ErrorPage from './pages/Core/Error'
 import Example from './pages/Example/Example'
+import PokemonPage from './pages/Pokemon/Index'
 import { LogsProvider } from './providers/Logs'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Example />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/pokemon',
+    element: <PokemonPage />,
     errorElement: <ErrorPage />,
   },
 ])
