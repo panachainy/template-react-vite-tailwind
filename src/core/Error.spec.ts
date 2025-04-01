07d309a6515b7ed788d81ae5d02227e0426910a8
Panachai <panachai.ny@gmail.com>
2025-04-02 00:28:16 +0700

    Update Error.spec.ts

--- src/core/Error.spec.ts
+++ src/core/Error.spec.ts
@@ -1,7 +1,10 @@
 import { expect, it } from 'vitest'
 import ErrorPage from './Error'
+import { describe } from 'node:test'
 
-it('toUpperCase', () => {
-  const result = ErrorPage('foobar')
+// describe('ErrorPage Component', () => {
+it('should render correctly and match the snapshot', () => {
+  const result = ErrorPage({})
   expect(result).toMatchSnapshot()
 })
+// })
